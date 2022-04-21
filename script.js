555f9b53032a5660973f91715bcdbcdaf06064e1
Diptesh Tarafdar <diptesh.tarafdar@gmail.com>
2022-04-21 19:41:43 +0530

    fixed score updation in js file

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const result = document.querySelector('.result');
 
 const options = document.querySelectorAll('.form-check');
 
-const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
 
 form.addEventListener('submit', e => {
     e.preventDefault();

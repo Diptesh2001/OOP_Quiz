1edf0030a3a528d9ff1f95695d2af27785749796
Diptesh Tarafdar <diptesh.tarafdar@gmail.com>
2022-04-23 10:17:55 +0530

    added score animation, score bug fixes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
 
     // check answers
     userAnswers.forEach((answer, index) => {
@@ -32,8 +32,20 @@ form.addEventListener('submit', e => {
     }
     
     scrollTo(0,0);
-    result.querySelector('span').textContent = `${score}%`;
+    
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+    
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+
+    }, 30)
 });
 
 
